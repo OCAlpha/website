74b23dc70b947a986afd022cc8bdf3f8aefedf39
Tommy Winberry <tommy.winberry@eagles.oc.edu>
2013-04-13 19:57:59 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fullrun::Application.config.secret_token = '890b7ac62513e6301aa7d4fbbd025b0bf690b14cdfdd23ad45b8079ae668c596fbf50c88b10f78f257f6d11b740719a0854f3d9256b50ac4532db3af3bcb6e4e'
