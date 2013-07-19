dd1e88a8a998ce2efc5631c21f6b858a2ce299d9
Tommy Winberry <tommy.winberry@eagles.oc.edu>
2013-07-19 18:25:33 -0500

    Update deleted items

--- db/migrate/20130420124650_create_reconciliations.rb
+++
@@ -1,9 +0,0 @@
-class CreateReconciliations < ActiveRecord::Migration
-  def change
-    create_table :reconciliations do |t|
-      t.decimal :balance, :scale => 2
-
-      t.timestamps
-    end
-  end
-end
