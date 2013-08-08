112e24ed3efd5c3c8fa4c1b14e6df0df226e2a55
Tommy Winberry <tomster2012@gmail.com>
2013-08-08 13:48:38 -0500

    Add balance function to Account.rb

--- app/models/account.rb
+++ app/models/account.rb
@@ -15,9 +15,65 @@ class Account < ActiveRecord::Base
   has_many :xfers_out, :class_name => 'AccountTransfer', :foreign_key => "to_account_id"
   has_many :reconciliations
   def balance
-    @lastTotal = self.reconciliations.last
-    @transfers = Transfer.where("account_id = ?", self.id)
-    "0.00"
-  end
+    @lastReconciliation = self.reconciliations.last
+    balance = 0
+    if(@lastReconciliation.nil?)
+      
+      @deposits = Transfer.where("account_id = ? ", self.id)
+      if(@deposits.nil?)
+        @deposits = 0
+      else
+        @deposits.each do |d|
+          balance += d.value
+        end
+      end
   
+      @transfers_out = AccountTransfer.where("from_account_id = ?", self.id)
+      if(@transfers_out.nil?)
+        @transfers_out = 0
+      else
+        @transfers_out.each do |t|
+          balance -= t.amount
+        end
+      end
+  
+      @transfers_in = AccountTransfer.where("to_account_id = ?", self.id)
+      if(@transfers_in.nil?)
+        @transfers_in = 0
+      else
+        @transfers_in.each do |t|
+          balance += t.amount
+        end
+      end
+    else
+      balance = @lastReconciliation.amount
+      @deposits = Transfer.where("account_id = ? AND created_at = ?", self.id, @lastReconciliation.created_at..Time.now)
+      if(@deposits.nil?)
+        @deposits = 0
+      else
+        @deposits.each do |d|
+          balance += d.value
+        end
+      end
+  
+      @transfers_out = AccountTransfer.where("from_account_id = ? AND created_at = ?", self.id, @lastReconciliation.created_at..Time.now)
+      if(@transfers_out.nil?)
+        @transfers_out = 0
+      else
+        @transfers_out.each do |t|
+          balance -= t.amount
+        end
+      end
+  
+      @transfers_in = AccountTransfer.where("to_account_id = ? AND created_at = ?", self.id, @lastReconciliation.created_at..Time.now)
+      if(@transfers_in.nil?)
+        @transfers_in = 0
+      else
+        @transfers_in.each do |t|
+          balance += t.amount
+        end
+      end
+    end
+  return balance
+  end
 end
